0c9b5cc40e71fe5d60b7884f3bf050592100cfdc
Matthew Finkel <Matthew.Finkel@gmail.com>
2017-09-11 01:16:43 +0000

    Add a main function

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+5ycast:
+	g++ -Werror -g -std=c++11 -Iinclude -o 5ycast \
+	src/mdns_message.cc src/mdns_message_header.cc \
+	src/mdns_message_question.cc src/mdns_message_rr.cc \
+	src/mnet.cc src/main.cc
+
 tests:
 	g++ -Werror -g -std=c++11 -pthread -Iinclude \
 	-I../googletest/googletest/include/ -o test_dns_message \
---
+++ src/main.cc
@@ -0,0 +1,54 @@
+/*  5ycast - Google/chromecast implementation
+ *  Copyright (C) 2017  Matthew Finkel
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <cstdio>
+#include <string>
+
+#include "mdns_message.h"
+#include "mnet.h"
+
+int main()
+{
+  std::string errmsg;
+  mnet::MNet mnet;
+  if (!mnet.CreateSocket(errmsg)) {
+    printf("CreateSocket() failed: %s\n", errmsg.c_str());
+    return -1;
+  }
+  if (!mnet.DisableMulticastLoop(errmsg)) {
+    printf("DisableMulticastLoop() failed: %s\n", errmsg.c_str());
+    return -1;
+  }
+  if (!mnet.AddMulticastMembership(errmsg)) {
+    printf("AddMulticastMembership() failed: %s\n", errmsg.c_str());
+    return -1;
+  }
+  printf("AddMulticastMembership() said: %s\n", errmsg.c_str());
+  if (!mnet.Poll(errmsg)) {
+    printf("Poll() failed: %s\n", errmsg.c_str());
+    return -1;
+  }
+
+  char* msgbuf;
+  size_t msgbuflen;
+  if (!mnet.Read(&msgbuf, msgbuflen, errmsg)) {
+    printf("Read() failed: %s\n", errmsg.c_str());
+    return -1;
+  }
+  printf("Read %lu bytes: '%s'\n\n", msgbuflen, msgbuf);
+  printf("Everything ran successfully!\n");
+  return 0;
+}
